fb281a4bd6a188efb9723b8fdd2eab4b9faf387a
Meghraj Jare <meghrajjare77@gmail.com>
2025-01-01 23:54:59 +0530

    deployement

--- src/app/profile/user-session.tsx
+++ src/app/profile/user-session.tsx
@@ -1,24 +1,24 @@
-'use client'
+// 'use client'
 
-import { useState, useEffect } from 'react';
-import { authClient } from "@/lib/authClient";
-import ClientPage from './profile-page';
+// import { useState, useEffect } from 'react';
+// import { authClient } from "@/lib/authClient";
+// import ClientPage from './profile-page';
 
-export default function UserSession() {
-  const [isLoading, setIsLoading] = useState(true);
-  const session = authClient.useSession();
+// export default function UserSession() {
+//   const [isLoading, setIsLoading] = useState(true);
+//   const session = authClient.useSession();
 
-  useEffect(() => {
-    if (session !== undefined) {
-      console.log(session);
-      setIsLoading(false);
-    }
-  }, [session]);
+//   useEffect(() => {
+//     if (session !== undefined) {
+//       console.log(session);
+//       setIsLoading(false);
+//     }
+//   }, [session]);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+//   if (isLoading) {
+//     return <div>Loading...</div>;
+//   }
 
-  return <ClientPage session={session} />;
-}
+//   return <ClientPage session={session} />;
+// }
 
