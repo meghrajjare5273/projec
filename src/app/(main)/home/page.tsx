328da201fa3a99c26357dd6c156922c90de0bfeb
Meghraj Jare <meghrajjare77@gmail.com>
2025-01-10 21:35:07 +0530

    ShadCN Sidebar V2

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -5,20 +5,20 @@ import { userSession } from "@/lib/types";
 import { useEffect, useState } from "react";
 
 export default function Homepage() {
-//   const [user, setUser] = useState<userSession | undefined>(undefined);
-//   useEffect(() => {
-//     async function fetchUser() {
-//       const user = await currentUser();
-//       if (user) {
-//         setUser(user);
-//       }
-//     }
-//     fetchUser();
-//   }, []);
+  const [user, setUser] = useState<userSession | undefined>(undefined);
+  useEffect(() => {
+    async function fetchUser() {
+      const user = await currentUser();
+      if (user) {
+        setUser(user);
+      }
+    }
+    fetchUser();
+  }, []);
 
   return(
     <>
-      <h1>Welcome</h1>
+      <h1>Welcome {user?.name}</h1>
     </>
   )
 }
