b035e8993a0e0cb445e0c23c4bd653ad092a10c1
Meghraj Jare <meghrajjare77@gmail.com>
2025-01-11 00:59:13 +0530

    Fixed TypeScript Errors

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -41,8 +41,10 @@ export function NavUser({ user }: { user: userSessionSidebar | null}) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user?.image} alt={user?.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                <AvatarImage src={user?.image || undefined} alt={user?.name || undefined} />
+                <AvatarFallback className="rounded-lg">
+                  {user?.name ? user.name.charAt(0).toUpperCase() : "U"}
+                </AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user?.name}</span>
@@ -60,7 +62,7 @@ export function NavUser({ user }: { user: userSessionSidebar | null}) {
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.image} alt={user?.name} />
+                  <AvatarImage src={user?.image || undefined} alt={user?.name || undefined} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
