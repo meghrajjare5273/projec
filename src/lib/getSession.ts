fefc04c79676e4f54b3afa815f70a63609451301
Meghraj Jare <meghrajjare77@gmail.com>
2025-01-12 13:45:14 +0530

    Redis Database For Session Caching

--- src/lib/getSession.ts
+++ src/lib/getSession.ts
@@ -1,28 +1,40 @@
-"use server"
+"use server";
 
+import { Redis } from "@upstash/redis";
 import { headers } from "next/headers";
 import { auth } from "./auth";
 import { redirect } from "next/navigation";
-import prisma from "./prisma";
+import prisma from "./prisma"
 
-export  async function userSession(){
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
-    if(!session){
-        return redirect("/")
-    }else{
-        return session?.user
-    }
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
+
+export async function userSession() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session) {
+    return redirect("/");
+  } else {
+    return session?.user;
+  }
 }
 
-export const currentUser = async () =>{
-    const userData = await userSession()
+export const currentUser = async () => {
+  const userData = await userSession();
+  const userRedis = await redis.get(userData.id);
+  if (!userRedis) {
     const user = await prisma.user.findFirst({
-        where:{
-          id: userData.id as string
-        },
-        cacheStrategy: {swr:60, ttl:30}
-      })
-    return user  
-} 
+      where: {
+        id: userData.id as string,
+      },
+      cacheStrategy: { swr: 60, ttl: 30 },
+    });
+    await redis.set(userData.id, user);
+    return user;
+  } else {
+    return userRedis;
+  }
+};
